5368d1cd8a5b9496ddf322f7012737d80a46ff19
aryan2121 <kayoukaan@gmail.com>
2019-10-05 14:04:25 +0530

    openInstitutes and category xml

---
+++ app/src/main/java/com/example/user/coachingdhundho/openInstitute.kt
@@ -0,0 +1,12 @@
+package com.example.user.coachingdhundho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class openInstitute : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_institute)
+    }
+}
